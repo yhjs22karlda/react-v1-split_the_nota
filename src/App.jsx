df46fb84a3f9b190a558719d54f9301303f6156d
Daniel Karlsson <danielo_o@hotmail.com>
2023-01-28 14:26:00 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="app">
+      <h1>Split the nota</h1>
+      <section className="container">
+        <p>Summa</p>
+        <input />
+        <p>Antal vänner</p>
+        <input />
+        <p>Dricks</p>
+        <input />
+        <button>Räkna</button>
+      </section>
+    </div>
+  )
+}
+
+export default App
