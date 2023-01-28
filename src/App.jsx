4c4dd97032f793d631ae14527e4c2cc6fea78676
Daniel Karlsson <danielo_o@hotmail.com>
2023-01-28 17:18:46 +0100

    logic added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
 import './App.css'
 
 function App() {
+  let sum = 0;
+  let numberOfFriends = 1;
+  let tip = 0;
+
+  function calculateTip() {
+    let toPay = ((sum * (1 + tip)) / numberOfFriends).toFixed(2);
+    console.log(`Varje person ska betala ${Number(toPay)} kr`); // Number() tar bort decimalerna om dom är nollor
+  }
 
   return (
     <div className="app">
       <h1>Split the nota</h1>
       <section className="container">
         <p>Summa</p>
-        <input />
+        <input onChange={(e) => {sum = Number(e.target.value)}}/>
         <p>Antal vänner</p>
-        <input />
+        <input onChange={(e) => {numberOfFriends = Number(e.target.value)}}/>
         <p>Dricks</p>
-        <input />
-        <button>Räkna</button>
+        <input onChange={(e) => {tip = Number(e.target.value)}}/>
+        <button onClick={calculateTip}>Räkna</button>
       </section>
     </div>
   )
